dacf5366488415f3d97d0eac30029bcdc5c12081
Justin G <MBA13-JG@Justins-MacBook-Air.local>
2017-07-15 03:26:27 -0500

    instructions - stopped at liri js

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'hfpSohCN7RmABDjmRDE5vE8tp',
+  consumer_secret: 'sGLpMaJ7kxfmEI5775DrE5XWLTAUl5TnE8SBljElZYoiSr47UQ',
+  access_token_key: '275775665-vSboWcfg8uzzpTRZvjh6MnbLTWAvd1ZKvJb9NhxB',
+  access_token_secret: 'yNgGvuZQwBMDTAa1G0qU78DLpvhx5BnhXDEcs6uI24Bju',
+}
