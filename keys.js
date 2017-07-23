89c9eb866a727aadf2da32a226b775cade4e7c28
Justin G <MBA13-JG@Justins-MacBook-Air.local>
2017-07-23 12:55:25 -0500

    didnt want to expose keys but need it for grading purposes oops

---
+++ keys.js
@@ -0,0 +1,16 @@
+// REMOVE THESE KEYS FROM THE PUBLICCCCC
+
+// testing
+//console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'hfpSohCN7RmABDjmRDE5vE8tp',
+  consumer_secret: 'sGLpMaJ7kxfmEI5775DrE5XWLTAUl5TnE8SBljElZYoiSr47UQ',
+  access_token_key: '275775665-vSboWcfg8uzzpTRZvjh6MnbLTWAvd1ZKvJb9NhxB',
+  access_token_secret: 'yNgGvuZQwBMDTAa1G0qU78DLpvhx5BnhXDEcs6uI24Bju',
+}
+
+exports.spotifyKeys = {
+  id: 'fed5556100d141d8b0d796cff2f1b70d',
+  secret: '0280e6c3d7f84337b505d85a62a96a6d'
+}
